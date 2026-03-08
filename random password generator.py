4dbdc3cd2f8be7be8e72c0a4e1db50a6adb9dab3
saibalaji1233 <bommadevarasaibalaji@gmail.com>
2026-03-08 17:43:42 +0530

    Update random password generator.py

--- random password generator.py
+++ random password generator.py
@@ -1,25 +1,25 @@
 import random
 import string
 def generate_password():
-    # ask user for password preferences
-    length = int(input("Enter password length: "))
-    upper = input("Do you want uppercase letters? (yes/no): ").lower()
-    special = input("Do you want special characters? (yes/no): ").lower()
-    digits = input("Do you want digits? (yes/no): ").lower()
-    # basic characters (small letters)
-    characters = string.ascii_lowercase
-    # add other characters if user wants
-    if upper == "yes":
-        characters += string.ascii_uppercase
-    if special == "yes":
-        characters += string.punctuation
-    if digits == "yes":
-        characters += string.digits
-    # generate password
-    password = ""
+# ask user for password preferences
+    length=int(input("Enter password length:"))
+    upper=input("Do you want uppercase letters?(yes/no):").lower()
+    special=input("Do you want special characters?(yes/no):").lower()
+    digits=input("Do you want digits?(yes/no):").lower()
+# basic characters (small letters)
+    characters=string.ascii_lowercase
+# add other characters if user wants
+    if upper=="yes":
+        characters+=string.ascii_uppercase
+    if special=="yes":
+        characters+=string.punctuation
+    if digits=="yes":
+        characters+=string.digits
+# generate password
+    password=""
     for i in range(length):
-        password += random.choice(characters)
+        password+=random.choice(characters)
     return password
 # call the function
-result = generate_password()
-print("Generated Password:", result)
+result=generate_password()
+print("Generated Password:",result)
